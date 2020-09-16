b6607801009c8795f66294efc1b8bfcacf1f110c
Adarsha <learnadarsha@gmail.com>
2020-09-16 16:30:35 +0545

    Edit model

--- SalesTransaction.Application.Model/Account/MvAccount.cs
+++ SalesTransaction.Application.Model/Account/MvAccount.cs
@@ -7,12 +7,8 @@ namespace SalesTransaction.Application.Model.Account
     class MvAccount
     {
     }
-    public class MvUser
+    public class MvLogin
     {
-        public int UserId { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
