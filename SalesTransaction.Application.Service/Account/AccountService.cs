6ae4f25f08c8bdc7706837276631b21c2b6d85ab
Adarsha <learnadarsha@gmail.com>
2020-09-16 17:44:45 +0545

    Connect data access with service

--- SalesTransaction.Application.Service/Account/AccountService.cs
+++ SalesTransaction.Application.Service/Account/AccountService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 using SalesTransaction.Application.DataAccess.Account;
 using SalesTransaction.Application.Model.Account;
 
@@ -10,5 +11,33 @@ namespace SalesTransaction.Application.Service.Account
     {
         private  DataAccessHelper _dataAccess;
         private readonly string _connectionString;
+        private readonly int _commandTimeout;
+        private IConfiguration _configuraiton;
+
+        public AccountService(IConfiguration configuration)
+        {
+            _configuraiton = configuration;
+            dynamic connectionString = _configuraiton.GetSection("ConnectionString");
+            _connectionString = connectionString["DefaultConnection"];
+
+            if (_connectionString != null)
+            {
+                _dataAccess = new DataAccessHelper(_connectionString);
+            }
+
+            _commandTimeout = Convert.ToInt32(connectionString["CommandTimeout"]);
+        }
+
+
+        public dynamic GetLogin(MvLogin login)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                var command = connection.CreateCommand();
+            }
+
+            return 0;
+        }
+
     }
 }
