50fed7cded6f26e3ff9b3d59e8da89b31495ea49
Adarsha <learnadarsha@gmail.com>
2020-09-17 23:51:36 +0545

    Add required in model

--- SalesTransaction.Application.Model/Account/MvAccount.cs
+++ SalesTransaction.Application.Model/Account/MvAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SalesTransaction.Application.Model.Account
@@ -9,7 +10,10 @@ namespace SalesTransaction.Application.Model.Account
     }
     public class MvLogin
     {
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
